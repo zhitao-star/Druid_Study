e95a29e0419029fc3289f8b6072ae76012ffaef8
小王同学 <1542344316@qq.com>
2023-02-20 20:38:52 +0800

    Initial commit

---
+++ src/main/java/top/wangzhitao/druid_study/DruidStudyApplication.java
@@ -0,0 +1,12 @@
+package top.wangzhitao.druid_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidStudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DruidStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/wangzhitao/druid_study/DruidStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package top.wangzhitao.druid_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DruidStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
